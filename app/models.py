19edeb5e164c6ad4ef949aed85326426d619f347
Nickson-Simiyu <simiyunickson1@gmail.com>
2024-03-03 13:02:28 +0300

    updated the models for login

--- app/models.py
+++ app/models.py
@@ -20,13 +20,11 @@ class CustomerOrder(db.Model):
     time = db.Column(db.DateTime, nullable=False)
 
 class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
-    username = db.Column(db.String(80), unique=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
 
-    def __init__(self, username, email, password):
-        self.username = username
+    def __init__(self, email, password):
         self.email = email
         self.password_hash = generate_password_hash(password)
 
